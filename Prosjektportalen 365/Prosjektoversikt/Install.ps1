c4cccfe72c95461e27410b05c183b32d48c99ce7
olemp <olemp@puzzlepart.com>
2020-05-06 17:33:46 +0200

    Minor update to Install script (WIP)

--- Prosjektportalen 365/Prosjektoversikt/Install.ps1
+++ Prosjektportalen 365/Prosjektoversikt/Install.ps1
@@ -1,8 +1,15 @@
 Param(
     [Parameter(Mandatory = $true, HelpMessage = "N/A")]
-    [string]$Url
+    [string]$Url,
+    [Parameter(Mandatory = $false, HelpMessage = "N/A")]
+    [string]$AppCatalogUrl
 )
 
-Connect-PnPOnline -Url $Url -UseWebLogin
-
-Apply-PnPProvisioningTemplate -Path .\template.xml
+if ($AppCatalogUrl) {
+    Connect-PnPOnline -Url $Url -UseWebLogin
+    Add-PnPApp -Path .\sharepoint\solution\pp-addons-prosjektoversikt.sppkg -Scope Tenant -Publish -Overwrite -SkipFeatureDeployment -ErrorAction Stop >$null 2>&1
+}
+else {
+    Connect-PnPOnline -Url $Url -UseWebLogin
+    Apply-PnPProvisioningTemplate -Path .\template.xml
+}
