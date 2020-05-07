29090d9e40d44acf2b61b89e13e265b541a0882e
olemp <olemp@puzzlepart.com>
2020-05-07 12:02:10 +0200

    Updated ProjectModel with a matchFilters prototype

--- Prosjektportalen 365/Prosjektoversikt/src/projectOverview/models/ProjectModel.ts
+++ Prosjektportalen 365/Prosjektoversikt/src/projectOverview/models/ProjectModel.ts
@@ -1,4 +1,5 @@
-import { first } from 'underscore';
+import { find, first } from 'underscore';
+import { contains } from 'underscore.string';
 import { IFilter } from '../components/FilterPanel';
 import { ProjectStatusModel } from './ProjectStatusModel';
 
@@ -65,11 +66,26 @@ export class ProjectModel {
         return this;
     }
 
+    /**
+     * Get the SharePoint item
+    */
     public getItem(): IProjectItem {
         return this.item;
     }
 
+    /**
+     * Check if the project matches the specified filters
+     * 
+     * @param {IFilter[]} filters Filters
+     */
     public matchFilters(filters: IFilter[]): boolean {
+        for (let i = 0; i < filters.length; i++) {
+            const filter = filters[i]
+            if (filter.selected.length === 0) continue;
+            const value = this.item[filter.key];
+            const match = find(filter.selected, s => contains(value, s.value));
+            if (!match) return false;
+        }
         return true;
     }
 }
