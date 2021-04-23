db9007a501c95160575c579789c84aa56314abc0
Rundez <38385042+Rundez@users.noreply.github.com>
2021-04-23 14:19:11 +0200

    Added command extension to Idébehandling list

---
+++ Prosjektideer/Extensions/Idebehandling/src/extensions/projectIdeaProcessing/ProjectIdeaProcessingCommandSet.ts
@@ -0,0 +1,124 @@
+import { override } from "@microsoft/decorators";
+import { Log } from "@microsoft/sp-core-library";
+import {
+  BaseListViewCommandSet,
+  Command,
+  IListViewCommandSetListViewUpdatedParameters,
+  IListViewCommandSetExecuteEventParameters,
+  RowAccessor,
+} from "@microsoft/sp-listview-extensibility";
+import { Dialog } from "@microsoft/sp-dialog";
+import { ClientsideText, sp } from "@pnp/sp/presets/all";
+import DialogPrompt from "./Components/Dialog";
+import { ConsoleListener, Logger, LogLevel } from "@pnp/logging";
+
+Logger.subscribe(new ConsoleListener());
+Logger.activeLogLevel = DEBUG ? LogLevel.Info : LogLevel.Warning;
+const LOG_SOURCE: string = "ProjectIdeaProcessCommandSet";
+
+enum RecommendationType {
+  Accepted = "Godkjent for konseptutredning",
+  Consideration = "Under vurdering",
+  Declined = "Avvist",
+}
+
+export default class ProjectIdeaProcessCommandSet extends BaseListViewCommandSet<any> {
+  private userAuthorized: boolean;
+
+  @override
+  public async onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, "Initialized ProjectIdeaProcessCommandSet");
+    this.userAuthorized = await this.isUserAuthorized();
+
+    return Promise.resolve();
+  }
+
+  @override
+  public async onListViewUpdated(
+    event: IListViewCommandSetListViewUpdatedParameters
+  ): Promise<any> {
+    const compareOneCommand: Command = this.tryGetCommand(
+      "RECOMMENDATION_COMMAND"
+    );
+    if (compareOneCommand) {
+      // This command should be hidden unless exactly one row is selected.
+      compareOneCommand.visible =
+        event.selectedRows.length === 1 && this.userAuthorized;
+    }
+  }
+
+  @override
+  public onExecute(event: IListViewCommandSetExecuteEventParameters): any {
+    switch (event.itemId) {
+      case "RECOMMENDATION_COMMAND":
+        Dialog.alert("");
+        const dialog: DialogPrompt = new DialogPrompt();
+
+        dialog.ideaTitle = event.selectedRows[0].getValueByName("Title");
+        dialog.show().then(() => {
+          if (dialog.comment && dialog.selectedChoice == "Godkjenn") {
+            this.onSubmit(
+              event.selectedRows[0],
+              dialog.comment,
+              dialog.selectedChoice
+            );
+          } else if (
+            dialog.comment &&
+            dialog.selectedChoice == "Under vurdering"
+          ) {
+            this.onSubmitConsideration(event.selectedRows[0], dialog.comment);
+          } else {
+            Logger.log({ message: "Declined", level: LogLevel.Info });
+          }
+        });
+        break;
+      default:
+        throw new Error("Unknown command");
+    }
+  }
+
+  private async onSubmitConsideration(
+    selectedRow: RowAccessor,
+    recComment: string
+  ) {
+    const rowId = selectedRow.getValueByName("ID");
+    sp.web.lists
+      .getByTitle("Idébehandling")
+      .items.getById(rowId)
+      .update({
+        GtIdeaRecommendation: RecommendationType.Consideration,
+        GtIdeaRecommendationComment: recComment,
+      })
+      .then(() => console.log("Updated Idébehandling"));
+  }
+
+  /**
+   * When submit button of the dialog is pressed fields will be updated, written to a new list, then a sitepage will be created
+   */
+  private async onSubmit(
+    selectedRow: RowAccessor,
+    recComment: string,
+    recChoice: string
+  ) {
+    const rowId = selectedRow.getValueByName("ID");
+    const rowTitle = selectedRow.getValueByName("Title");
+    sp.web.lists
+      .getByTitle("Idébehandling")
+      .items.getById(rowId)
+      .update({
+        GtIdeaDecision: RecommendationType.Accepted,
+        GtIdeaDecisionComment: recComment,
+      })
+      .then(() => console.log("Updated Idébehandling"));
+  }
+
+  /**
+   * Checks if the current user has premisions to set recommendation
+   */
+  private async isUserAuthorized(): Promise<boolean> {
+    const users = await sp.web.siteGroups.getByName("Idebehandlere").users();
+    return users.some(
+      (user) => user.Email == this.context.pageContext.user.email
+    );
+  }
+}
