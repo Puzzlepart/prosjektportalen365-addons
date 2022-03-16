d131c0ff63dba7d72ccab808c41a6d31287b011a
Ole Martin Pettersen <olemp@puzzlepart.com>
2022-03-16 15:39:25 +0100

    Removed livereload as gulp serve has built-in reload mechanisms

--- Prosjektoversikt/gulpfile.js
+++ Prosjektoversikt/gulpfile.js
@@ -11,17 +11,7 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-DetailsHeader-cell' is not camelCase and will not be type-safe.`)
-
-var getTasks = build.rig.getTasks;
-build.rig.getTasks = function () {
-  var result = getTasks.call(build.rig);
-
-  result.set('serve', result.get('serve-deprecated'));
-
-  return result;
-};
-
-
+build.addSuppression(`Warning - [sass] src/projectOverview/components/ProjectOverview/ProjectOverview.scss: filename should end with module.sass or module.scss`)
 
 /**
  * Custom Framework Specific gulp tasks
