e9329c61d1a1f1a6f6b9d6bd0d1f8236e4ae86f6
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2022-10-17 12:22:24 +0200

    Remove idea processing from prosjektportalen365-addons
    This is now included directly with PP365 installation

--- Prosjektideer/SharePointFramework/Extensions/ProjectData/src/extensions/projectData/loc/myStrings.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface IProjectDataCommandSetStrings {
-  Command1: string;
-  Command2: string;
-}
-
-declare module 'ProjectDataCommandSetStrings' {
-  const strings: IProjectDataCommandSetStrings;
-  export = strings;
-}
