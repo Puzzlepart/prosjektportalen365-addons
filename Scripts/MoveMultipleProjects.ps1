fb7bb7ae14d87f52664b244034b0f3b9ee1fd367
Tarjei E. Ormestøyl <ormestoyl@gmail.com>
2023-02-10 12:10:37 +0100

    Script to move multiple projects

---
+++ Scripts/MoveMultipleProjects.ps1
@@ -0,0 +1,22 @@
+$ProjectsToMove = @(
+"https://xxx.sharepoint.com/sites/project1",
+"https://xxx.sharepoint.com/sites/project2",
+"https://xxx.sharepoint.com/sites/project3")
+
+
+Start-Transcript -Path "$PSScriptRoot/MoveSites_Log-$((Get-Date).ToString('yyyy-MM-dd-HH-mm')).txt"
+
+try { 
+    $AzureADCommand = Get-AzureADTenantDetail 
+} 
+catch [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] { 
+    Write-Host "Connecting to Azure AD" 
+    $AzureConnection = Connect-AzureAD 
+}
+
+$ProjectsToMove | ForEach-Object {
+    .\MoveProjectBetweenHubs.ps1 -SourceHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen" -DestinationHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen365" -ProjectUrl $_
+}
+
+Disconnect-AzureAD
+Stop-Transcript
