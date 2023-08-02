8d1fe7aa771e355004569e6403ef328fbdb60d96
Tarjei E. Ormestøyl <ormestoyl@gmail.com>
2023-08-02 08:51:52 +0200

    try-catch-finally to stop transcript

--- Scripts/MoveMultipleProjects.ps1
+++ Scripts/MoveMultipleProjects.ps1
@@ -1,22 +1,28 @@
 $ProjectsToMove = @(
-"https://xxx.sharepoint.com/sites/project1",
-"https://xxx.sharepoint.com/sites/project2",
-"https://xxx.sharepoint.com/sites/project3")
+    "https://xxx.sharepoint.com/sites/project1",
+    "https://xxx.sharepoint.com/sites/project2",
+    "https://xxx.sharepoint.com/sites/project3")
 
 
 Start-Transcript -Path "$PSScriptRoot/MoveSites_Log-$((Get-Date).ToString('yyyy-MM-dd-HH-mm')).txt"
 
-try { 
-    $AzureADCommand = Get-AzureADTenantDetail 
-} 
-catch [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] { 
-    Write-Host "Connecting to Azure AD" 
-    $AzureConnection = Connect-AzureAD 
-}
+try {
+    try { 
+        $AzureADCommand = Get-AzureADTenantDetail 
+    } 
+    catch [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] { 
+        Write-Host "Connecting to Azure AD" 
+        $AzureConnection = Connect-AzureAD 
+    }
+    $ProjectsToMove | ForEach-Object {
+        .\MoveProjectBetweenHubs.ps1 -SourceHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen" -DestinationHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen365" -ProjectUrl $_
+    }
 
-$ProjectsToMove | ForEach-Object {
-    .\MoveProjectBetweenHubs.ps1 -SourceHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen" -DestinationHubUrl "https://xxx.sharepoint.com/sites/Prosjektportalen365" -ProjectUrl $_
+    Disconnect-AzureAD
 }
-
-Disconnect-AzureAD
-Stop-Transcript
+catch {
+    Write-Host "An error occured: $($_.Exception.Message)"
+}
+finally {
+    Stop-Transcript
+}
