53a26e77e2b34b4e1af047257dfe5a0dac4d3504
Remi Blom-Ohlsen <remi.blom-ohlsen@puzzlepart.com>
2024-06-21 09:51:16 +0200

    [Prosjektoversikt] Updated template schema and PnP.PowerShell

--- Prosjektoversikt/Install.ps1
+++ Prosjektoversikt/Install.ps1
@@ -3,25 +3,18 @@ Param(
     [string]$Url
 )
 
-$Command = Get-Command connect-pnponline
-
-if ($null -eq $Command -or $Command.Source -ne "SharePointPnPPowerShellOnline") {
-    Write-Host "SharePointPnPPowerShellOnline is not loaded. Unable to continue" -ForegroundColor Red
-    return
-}
-
 Set-PnPTraceLog -Off
 
-Connect-PnPOnline -Url $Url -UseWebLogin
+Connect-PnPOnline -Url $Url -Interactive
 $AppCatalogUrl = Get-PnPTenantAppCatalogUrl
 
 Write-Host "Adding app to the tenant app catalog"
-Connect-PnPOnline -Url $AppCatalogUrl -UseWebLogin
-$App = Add-PnPApp -Path .\pp-addons-prosjektoversikt.sppkg -Scope Tenant -Publish -Overwrite -SkipFeatureDeployment -ErrorAction Stop
+Connect-PnPOnline -Url $AppCatalogUrl --Interactive
+$App = Add-PnPApp -Path ./pp-addons-prosjektoversikt.sppkg -Scope Tenant -Publish -Overwrite -SkipFeatureDeployment -ErrorAction Stop
 
 Write-Host "Adding page and app to $Url"
-Connect-PnPOnline -Url $Url -UseWebLogin
-Apply-PnPProvisioningTemplate -Path .\Templates\Template.xml
+Connect-PnPOnline -Url $Url -Interactive
+Invoke-PnPSiteTemplate -Path ./Templates/Template.xml
 
 Write-Host "Installation completed. The app is available at $($Url + "/SitePages/Prosjektoversikt.aspx")"
 Write-Host "Verify that the correct portfolio(s) is configured at $($Url + "/Lists/Prosjektoversiktkonfigurasjon/AllItems.aspx")"
